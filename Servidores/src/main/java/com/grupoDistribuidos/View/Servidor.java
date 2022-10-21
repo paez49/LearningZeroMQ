873432fe6d5cfa921d331084172598edea660865
paez49 <jdpluc302@gmail.com>
2022-10-20 22:16:34 -0500

    Has agregado

--- Servidores/src/main/java/com/grupoDistribuidos/View/Servidor.java
+++ Servidores/src/main/java/com/grupoDistribuidos/View/Servidor.java
@@ -11,6 +11,15 @@ import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ.Socket;
 
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
 public class Servidor {
 
     public static void main(String[] argv) throws Exception {
@@ -24,9 +33,10 @@ public class Servidor {
             fco.actualizarProducto(prod.getIdProducto(), prod.getCantiProducto());
             if (true) {
                 System.out.println("Se modificó");
-                System.out.println("PRODUCTO: "+prod.toString());
+                System.out.println("PRODUCTO: " + prod.toString());
             }
         }
+
         /*
          * try (ZContext context = new ZContext()) {
          * Socket server = context.createSocket(SocketType.REP);
@@ -42,6 +52,83 @@ public class Servidor {
          * }
          * }
          */
+        String originalPassword = "eskere";
 
+        String generatedSecuredPasswordHash = generateStorngPasswordHash(originalPassword);
+        System.out.println(generatedSecuredPasswordHash);
+        //String originalPassword = "password";
+        boolean matched = validatePassword("password", generatedSecuredPasswordHash);
+        System.out.println(matched);
+
+        matched = validatePassword("eskere", generatedSecuredPasswordHash);
+        System.out.println(matched);
+        generatedSecuredPasswordHash = generateStorngPasswordHash(originalPassword);
+        System.out.println(generatedSecuredPasswordHash);
+
+        matched = validatePassword("password", generatedSecuredPasswordHash);
+        System.out.println(matched);
+
+        matched = validatePassword("eskere", generatedSecuredPasswordHash);
+        System.out.println(matched);
+    }
+
+    private static String generateStorngPasswordHash(String password)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        int iterations = 1000;
+        char[] chars = password.toCharArray();
+        byte[] salt = getSalt();
+
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, iterations, 64 * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return iterations + ":" + toHex(salt) + ":" + toHex(hash);
+    }
+
+    private static byte[] getSalt() throws NoSuchAlgorithmException {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+
+    private static String toHex(byte[] array) throws NoSuchAlgorithmException {
+        BigInteger bi = new BigInteger(1, array);
+        String hex = bi.toString(16);
+
+        int paddingLength = (array.length * 2) - hex.length();
+        if (paddingLength > 0) {
+            return String.format("%0" + paddingLength + "d", 0) + hex;
+        } else {
+            return hex;
+        }
+    }
+
+    private static boolean validatePassword(String originalPassword, String storedPassword)
+            throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String[] parts = storedPassword.split(":");
+        int iterations = Integer.parseInt(parts[0]);
+
+        byte[] salt = fromHex(parts[1]);
+        byte[] hash = fromHex(parts[2]);
+
+        PBEKeySpec spec = new PBEKeySpec(originalPassword.toCharArray(),
+                salt, iterations, hash.length * 8);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] testHash = skf.generateSecret(spec).getEncoded();
+
+        int diff = hash.length ^ testHash.length;
+        for (int i = 0; i < hash.length && i < testHash.length; i++) {
+            diff |= hash[i] ^ testHash[i];
+        }
+        return diff == 0;
+    }
+
+    private static byte[] fromHex(String hex) throws NoSuchAlgorithmException {
+        byte[] bytes = new byte[hex.length() / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+        }
+        return bytes;
     }
 }
