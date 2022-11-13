23b264b003b363dcfccd422028f5d7446543f71a
paez49 <jdpluc302@gmail.com>
2022-11-12 19:58:53 -0500

    Balanceador funcionando

--- Balanceador/src/main/java/com/grupoDistribuidos/ProxyBalanceador.java
+++ Balanceador/src/main/java/com/grupoDistribuidos/ProxyBalanceador.java
@@ -8,78 +8,155 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Poller;
 import org.zeromq.ZMQ.Socket;
 import org.zeromq.ZContext;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.zeromq.*;
+import org.zeromq.ZMQ.Poller;
+import org.zeromq.ZMQ.Socket;
 public class ProxyBalanceador {
-    public static void main(String[] args) throws Exception {
-        try (ZContext context = new ZContext()) {
-            System.out.println("INICIANDO PROXY");
-            Socket frontend = context.createSocket(SocketType.ROUTER);
-            Socket backend = context.createSocket(SocketType.ROUTER);
-            frontend.bind("tcp://*:5559");
-            backend.bind("tcp://*:5560");
+    private final static int HEARTBEAT_LIVENESS = 3;    //  3-5 is reasonable
+    private final static int HEARTBEAT_INTERVAL = 1000; //  msecs
 
-            Queue<String> colaServidores = new LinkedList<String>();
-            while (true) {
-                Poller items = context.createPoller(2);
-                items.register(backend, Poller.POLLIN);
-                if (colaServidores.size() > 0) {
-                    items.register(frontend, Poller.POLLIN);
-                }
-                if (items.poll() < 0) {
+    //  Paranoid Pirate Protocol constants
+    private final static String PPP_READY     = "\001"; //  Signals worker is ready
+    private final static String PPP_HEARTBEAT = "\002"; //  Signals worker heartbeat
+
+    //  Here we define the worker class; a structure and a set of functions that
+    //  as constructor, destructor, and methods on worker objects:
+    private static class Worker
+    {
+        ZFrame address;  //  Address of worker
+        String identity; //  Printable identity
+        long   expiry;   //  Expires at this time
+
+        protected Worker(ZFrame address)
+        {
+            this.address = address;
+            identity = new String(address.getData(), ZMQ.CHARSET);
+            expiry = System.currentTimeMillis() + HEARTBEAT_INTERVAL * HEARTBEAT_LIVENESS;
+        }
+
+        //  The ready method puts a worker to the end of the ready list:
+        protected void ready(ArrayList<Worker> workers)
+        {
+            Iterator<Worker> it = workers.iterator();
+            while (it.hasNext()) {
+                Worker worker = it.next();
+                if (identity.equals(worker.identity)) {
+                    it.remove();
                     break;
                 }
-                if (items.pollin(0)) {
+            }
+            workers.add(this);
+        }
 
-                    // Queue worker address for LRU routing
-                    colaServidores.add(backend.recvStr());
+        //  The next method returns the next available worker address:
+        protected static ZFrame next(ArrayList<Worker> workers)
+        {
+            Worker worker = workers.remove(0);
+            assert (worker != null);
+            ZFrame frame = worker.address;
+            return frame;
+        }
 
-                    // Second frame is empty
-                    String empty = backend.recvStr();
-                    assert (empty.length() == 0);
-
-                    // Third frame is READY or else a client reply address
-                    String clientAddr = backend.recvStr();
-
-                    // If client reply, send rest back to frontend
-                    if (!clientAddr.equals("READY")) {
-
-                        empty = backend.recvStr();
-                        assert (empty.length() == 0);
-
-                        String reply = backend.recvStr();
-                        frontend.sendMore(clientAddr);
-                        frontend.sendMore("");
-                        frontend.send(reply);
-                    }
+        //  The purge method looks for and kills expired workers. We hold workers
+        //  from oldest to most recent, so we stop at the first alive worker:
+        protected static void purge(ArrayList<Worker> workers)
+        {
+            Iterator<Worker> it = workers.iterator();
+            while (it.hasNext()) {
+                Worker worker = it.next();
+                if (System.currentTimeMillis() < worker.expiry) {
+                    break;
                 }
-                if (items.pollin(1)) {
-                    // Now get next client request, route to LRU worker
-                    // Client request is [address][empty][request]
-                    String clientAddr = frontend.recvStr();
-
-                    String empty = frontend.recvStr();
-                    assert (empty.length() == 0);
-
-                    String request = frontend.recvStr();
-
-                    String workerAddr = colaServidores.poll();
-
-                    backend.sendMore(workerAddr);
-                    backend.sendMore("");
-                    backend.sendMore(clientAddr);
-                    backend.sendMore("");
-                    backend.send(request);
-                }
-
+                it.remove();
             }
         }
-        /*
-         * System.out.println("Iniciando Balanceador-Proxy");
-         * ZContext context = new ZContext();
-         * BackEnd backend = new BackEnd(context);
-         * FrontEnd frontend = new FrontEnd(context);
-         * frontend.start();
-         * backend.start();
-         */
+    };
+
+    //  The main task is an LRU queue with heartbeating on workers so we can
+    //  detect crashed or blocked worker tasks:
+    public static void main(String[] args)
+    {
+        try (ZContext ctx = new ZContext()) {
+            Socket frontend = ctx.createSocket(SocketType.ROUTER);
+            Socket backend = ctx.createSocket(SocketType.ROUTER);
+            frontend.bind("tcp://*:5555"); //  For clients
+            backend.bind("tcp://*:5556"); //  For workers
+
+            //  List of available workers
+            ArrayList<Worker> workers = new ArrayList<Worker>();
+
+            //  Send out heartbeats at regular intervals
+            long heartbeat_at = System.currentTimeMillis() + HEARTBEAT_INTERVAL;
+
+            Poller poller = ctx.createPoller(2);
+            poller.register(backend, Poller.POLLIN);
+            poller.register(frontend, Poller.POLLIN);
+
+            while (true) {
+                boolean workersAvailable = workers.size() > 0;
+                int rc = poller.poll(HEARTBEAT_INTERVAL);
+                if (rc == -1)
+                    break; //  Interrupted
+
+                //  Handle worker activity on backend
+                if (poller.pollin(0)) {
+                    //  Use worker address for LRU routing
+                    ZMsg msg = ZMsg.recvMsg(backend);
+                    if (msg == null)
+                        break; //  Interrupted
+
+                    //  Any sign of life from worker means it's ready
+                    ZFrame address = msg.unwrap();
+                    Worker worker = new Worker(address);
+                    worker.ready(workers);
+
+                    //  Validate control message, or return reply to client
+                    if (msg.size() == 1) {
+                        ZFrame frame = msg.getFirst();
+                        String data = new String(frame.getData(), ZMQ.CHARSET);
+                        if (!data.equals(PPP_READY) &&
+                            !data.equals(PPP_HEARTBEAT)) {
+                            System.out.println(
+                                "E: invalid message from worker"
+                            );
+                            msg.dump(System.out);
+                        }
+                        msg.destroy();
+                    }
+                    else msg.send(frontend);
+                }
+                if (workersAvailable && poller.pollin(1)) {
+                    //  Now get next client request, route to next worker
+                    ZMsg msg = ZMsg.recvMsg(frontend);
+                    if (msg == null)
+                        break; //  Interrupted
+                    msg.push(Worker.next(workers));
+                    msg.send(backend);
+                }
+
+                //  We handle heartbeating after any socket activity. First we
+                //  send heartbeats to any idle workers if it's time. Then we
+                //  purge any dead workers:
+                if (System.currentTimeMillis() >= heartbeat_at) {
+                    for (Worker worker : workers) {
+
+                        worker.address.send(
+                            backend, ZFrame.REUSE + ZFrame.MORE
+                        );
+                        ZFrame frame = new ZFrame(PPP_HEARTBEAT);
+                        frame.send(backend, 0);
+                    }
+                    long now = System.currentTimeMillis();
+                    heartbeat_at = now + HEARTBEAT_INTERVAL;
+                }
+                Worker.purge(workers);
+            }
+
+            //  When we're done, clean up properly
+            workers.clear();
+        }
     }
 }
