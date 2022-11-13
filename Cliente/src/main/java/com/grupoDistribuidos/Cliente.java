168df5b1f7022f8f115d7bfa22841e6255566cc3
Estudiante <jdpluc302@gmail.com>
2022-11-12 19:55:48 -0500

    Cliente funcionando

--- Cliente/src/main/java/com/grupoDistribuidos/Cliente.java
+++ Cliente/src/main/java/com/grupoDistribuidos/Cliente.java
@@ -7,6 +7,7 @@ import org.zeromq.SocketType;
 import org.zeromq.ZMQ.Poller;
 import org.zeromq.ZMQ.Socket;
 import org.zeromq.ZContext;
+import org.zeromq.ZMsg;
 
 public class Cliente {
 
@@ -15,16 +16,19 @@ public class Cliente {
             System.out.println("Ingrese peticion");
             String peticion = "hola";
             Scanner entradaEscaner = new Scanner (System.in); 
-            // peticion = entradaEscaner.nextLine();
+            // 
             Socket socketCliente = context.createSocket(SocketType.REQ);
             ZHelper.setId(socketCliente);
 
-            socketCliente.connect("tcp://25.63.93.84:5559");
-            String reply = "";
+            socketCliente.connect("tcp://25.63.93.84:5555");
+            int i =0;
             while(true){
+                peticion = entradaEscaner.nextLine();
                 socketCliente.send(peticion);
-                reply = socketCliente.recvStr();
-                System.out.println("Cliente: "+reply);
+                ZMsg reply = ZMsg.recvMsg(socketCliente);
+                System.out.println("Cliente: "+reply.toString());
+                System.out.println(i);
+                i++;
             }
             
         }
